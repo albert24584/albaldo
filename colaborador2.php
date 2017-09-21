a97f686510b88aa8c9135959f6b60540bab08b5e
asantamar87 <a4alumno@aules.local>
2017-09-21 10:16:26 +0200

    mensaje-aldo

---
+++ colaborador2.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Datos Entrenamiento</title>
+  </head>
+  <body>
+    <h1>Datos Entrenamiento</h1>
+    <table>
+      <tr>
+        <th>Corredor</th>
+        <th>Tiempo medio</th>
+        <th></th>
+      </tr>
+
+    </table>
+  </body>
+</html>
